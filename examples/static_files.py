7a008c4e69bf2c1b3caec1118faaff60c7da11ad
0x4aK <18755570+0x4aK@users.noreply.github.com>
2024-05-19 12:08:07 +0300

    chore(examples): update module doc

--- examples/static_files.py
+++ examples/static_files.py
@@ -2,7 +2,7 @@
 Example of serving static files.
 uWebServer will serve static files by default.
 The static folder can be specified when defining WebServer instance or
-disabled completely by passing `None`  as the static folder.
+disabled completely by passing `None` as the static folder.
 
 Static files can be plain files or GZip compressed.
 If the clients browser supports GZip encoding, that file will be sent.
